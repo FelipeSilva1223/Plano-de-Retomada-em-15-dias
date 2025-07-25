022be5d2f33aa073b2abc1e5a26979b33ff20832
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-24 23:20:00 -0300

    Adicionar arquivos de projeto.

---
+++ Dia1_Variaveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Dia1_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de IMC");
+            Console.WriteLine("Digite seu peso");
+            float peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            float imc = peso / (altura * altura);
+
+            Console.WriteLine("O seu IMC é igual a: " + imc);
+        }
+    }
+}
