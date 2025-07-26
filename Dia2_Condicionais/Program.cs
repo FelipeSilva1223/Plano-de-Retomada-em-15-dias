03a8cf14458a34933e82d37424a002be5dd13f15
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-25 23:57:56 -0300

    Bem simples por causa da falta de tempo (vou melhorar futuramente)

--- Dia2_Condicionais/Program.cs
+++ Dia2_Condicionais/Program.cs
@@ -4,9 +4,32 @@ namespace Dia2_Variaveis
 {
     class Program
     {
+        public static void VerificarFaixaEtaria()
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade <= 11)
+            {
+                Console.WriteLine("Você é criança");
+            }
+            else if (idade > 11 && idade < 18)
+            {
+                Console.WriteLine("Você é  adolescente");
+            }
+            else if (idade > 17 && idade < 59)
+            {
+                Console.WriteLine("Você é adulto");
+            }
+            else
+            {
+                Console.WriteLine("Você é idoso");
+            }
+            Console.Beep();
+        }
         static void Main(string[] args)
         {
-
+            VerificarFaixaEtaria();
         }
     }
 }
