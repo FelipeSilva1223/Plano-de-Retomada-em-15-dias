b4204ea137e7dd030b7b6366bd70fa1a3bac0e9f
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-26 22:18:28 -0300

    Função contar ate 10.

---
+++ Dia3_Lacos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dia3_lacos
+{
+    class Program
+    {
+        public static void ContarAte10()
+        {
+            int contador = 0;
+            while (contador < 10)
+            {
+                contador += 1;
+                Console.WriteLine($"{contador}");
+                Console.Beep();
+            }
+        }
+        static void Main(String[] args)
+        {
+            ContarAte10();
+        }
+    }
+}
