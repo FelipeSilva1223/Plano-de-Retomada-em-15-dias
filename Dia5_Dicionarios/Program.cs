c384d6cfdeeb2c38c6738cac71df5242bc8b9da4
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-28 23:47:25 -0300

    Primeiros passos com dicionários (Não conhecia nada sobre).

---
+++ Dia5_Dicionarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dia5_Dicionarios
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Dictionary<int, String> Agenda = new(); // Declaração de um dicionário
+
+            Agenda.Add(1, "Felipe"); // Adicionando valores ao dicinário
+            Agenda.Add(2, "Pet");
+            Agenda.Add(3, "Pirubola");
+            Agenda.Add(4, "Abu");
+
+            if (Agenda.ContainsKey(1)) // Declaração da Função ContainKey
+            {
+                Console.WriteLine(Agenda[1]);
+            }
+        }
+    }
+}
