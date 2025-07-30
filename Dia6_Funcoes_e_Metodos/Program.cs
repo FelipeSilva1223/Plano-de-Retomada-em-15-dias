5dd5aa1bef85ec51833fd1a8f2e7374a214b7e7e
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-29 22:11:46 -0300

    Dia 6

---
+++ Dia6_Funcoes_e_Metodos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dia6_Funcoes_e_Metodos
+{
+    class Program
+    {
+        public static String Saudar(string nome)
+        {
+            return $"Olá {nome}! Bem-vindo ao sistema.";
+        }
+        static void Main(String[] args)
+        {
+            string frase = Saudar("Felipe");
+            Console.WriteLine(frase);
+        }
+    }
+}
