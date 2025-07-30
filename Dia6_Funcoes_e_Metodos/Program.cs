ef58fabdbaa4e7a5ddaa674a922a72ddf45bc25c
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-29 22:35:07 -0300

    No método verificarMaioridade() eu fiz um if/else desnecessário, aprendi que >= tambem é um operador logico.

--- Dia6_Funcoes_e_Metodos/Program.cs
+++ Dia6_Funcoes_e_Metodos/Program.cs
@@ -8,10 +8,24 @@ namespace Dia6_Funcoes_e_Metodos
         {
             return $"Olá {nome}! Bem-vindo ao sistema.";
         }
+        public static bool verificarMaioridade(int idade)
+        {
+            return idade >= 18;
+        }
         static void Main(String[] args)
         {
-            string frase = Saudar("Felipe");
-            Console.WriteLine(frase);
+            /* string frase = Saudar("Felipe");
+            Console.WriteLine(frase); */
+            Console.WriteLine("Digite uma idade para verificar se está na maioridade");
+            int idade = int.Parse(Console.ReadLine());
+            bool maioridade = verificarMaioridade(idade);
+            if (maioridade)
+            {
+                Console.WriteLine("É de maior");
+            } else
+            {
+                Console.WriteLine("É de menor");
+            }
         }
     }
 }
