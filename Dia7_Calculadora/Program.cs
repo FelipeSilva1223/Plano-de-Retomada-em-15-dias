e542749f7de2327a7492a4224f2ed8afef63e13f
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-07-30 23:59:52 -0300

    menu

---
+++ Dia7_Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dia7_Calculadora
+{
+    class Program
+    {
+        public static int ExibirMenu(int opc)
+        {
+            Console.WriteLine("==== CALCULADORA ====");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("0 - Sair");
+            int acao = int.Parse(Console.ReadLine());
+            return acao;
+        }
+        static void Main(String[] args)
+        {
+            int opcao = 5;
+            while (opcao != 0)
+            {
+                int comando = ExibirMenu(opcao);
+            }
+        }
+    }
+}
