d9aad019e86469f575f66193ab0832f521538da2
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-08-06 00:23:03 -0300

    Mais classes

--- Dia9_Classes_E_Objetos/Program.cs
+++ Dia9_Classes_E_Objetos/Program.cs
@@ -5,26 +5,51 @@
         public string Nome;
         public int Idade;
 
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos!");
+        }
+    }
+
+    class Carro
+    {
+        public string Modelo;
+        public int Ano;
+        public string Cor;
+
+        public Carro(string modelo, int ano, string cor)
+        {
+            this.Modelo = modelo;
+            this.Ano = ano;
+            this.Cor = cor;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Este carro do modelo {Modelo}, é da cor {Cor} e foi fabricado no ano {Ano}");
         }
     }
     class Program
     {
         static void Main(String[] args)
         {
-            Pessoa p1 = new Pessoa();
-            p1.Nome = "Felipe";
-            p1.Idade = 26;
-            p1.Apresentar();
+            Pessoa p1 = new Pessoa("Felipe", 26);
+            Pessoa p2 = new Pessoa("Fulano", 17);
 
-            Pessoa p2 = new Pessoa
-            {
-                Nome = "Fulano",
-                Idade = 25
-            };
-            p2.Apresentar();
+            Carro c1 = new Carro("Fiat Uno", 2010, "Branca");
+            Carro c2 = new Carro("Fiat Palio", 2014, "Vermelha");
+            Carro c3 = new Carro("VW Gol GT", 1986, "Preta");
+            Carro c4 = new Carro("Ford Fiesta", 2006, "Prata");
+
+            c1.ExibirInformacoes();
+            c2.ExibirInformacoes();
+            c3.ExibirInformacoes();
+            c4.ExibirInformacoes();
         }
     }
 }
