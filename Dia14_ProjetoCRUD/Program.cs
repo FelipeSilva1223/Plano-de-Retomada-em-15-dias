8e826142d1620cc4a58c89e5cd3177743b2c9182
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-08-25 23:12:51 -0300

    Menu

---
+++ Dia14_ProjetoCRUD/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Dia14_ProjetoCRUD
+{
+    class Program
+    {
+        public static void ExibirMenu()
+        {
+            Console.WriteLine("=== GERENCIADOR DE TAREFAS ===");
+            Console.WriteLine("1 - Adicionar tarefa");
+            Console.WriteLine("2 - Listar tarefas");
+            Console.WriteLine("3 - Editar tarefa");
+            Console.WriteLine("4 - Excluir tarefa");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Escolha uma opção: ");
+
+        }
+        static void Main(String[] args)
+        {
+            ExibirMenu();
+        }
+    }
+}
