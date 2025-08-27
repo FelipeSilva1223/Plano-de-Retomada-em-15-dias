7adafe71ea6dd61ff739c2625c8adb4480dfb613
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2025-08-27 00:00:15 -0300

    Estrutura básica (sem ações)

--- Dia14_ProjetoCRUD/Program.cs
+++ Dia14_ProjetoCRUD/Program.cs
@@ -17,9 +17,46 @@
             return int.TryParse(entrada, out int opcao) ? opcao : -1;
 
         }
+        static void AdicionarTarefa()
+        {
+            List<string> tarefas = new List<string>();
+            foreach (string t in tarefas)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine("Adicinoar tarefa selecionado!");
+        }
+        static void ExibirTarefas()
+        {
+            Console.WriteLine("Exibir tarefa selecionado!");
+        }
+        static void EditarTarefas()
+        {
+            Console.WriteLine("Editar tarefa selecionado!");
+        }
+        static void ExcluirTarefa()
+        {
+            Console.WriteLine("Excluir tarefa selecionado!");
+        }
         static void Main(String[] args)
         {
-            ExibirMenu();
+            int opcao;
+
+            bool sair = false;
+
+            do
+            {
+                opcao = ExibirMenu();
+                
+                switch (opcao)
+                {
+                    case 1: AdicionarTarefa(); break;
+                    case 2: ExibirTarefas(); break;
+                    case 3: EditarTarefas(); break;
+                    case 4: ExibirTarefas(); break;
+                    case 0: sair = true; break;
+                }
+            } while (!sair);
         }
     }
 }
